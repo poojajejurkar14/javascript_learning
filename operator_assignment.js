dd533b0c3c09719ca46c92dfbf1b38f42b96c909
277889 <112204285+277889@users.noreply.github.com>
2022-10-14 18:00:13 +0530

    operator_assignment
    operator_assignment

---
+++ operator_assignment.js
@@ -0,0 +1,33 @@
+var wordLengthSquare=function(arg){
+    var result=arg.length;
+    var square=result*result;
+    console.log(`Given word is: ${arg}`);
+    console.log(`length of word: ${result}`);
+    console.log(`square of word: ${square}`);
+    console.log("*******************************************");
+
+
+}
+wordLengthSquare("JavaScript");
+wordLengthSquare("Google");
+wordLengthSquare("Developer");
+
+function sentence(){
+    var givenstr="I Am Angular Developer";
+    console.log(givenstr);
+    console.log(`string lenrgth: ${givenstr.length}`);
+    var result=givenstr.split(" ");
+    console.log(result);
+    var total_word=result.length;
+    console.log(`string length divided by total words: ${givenstr.length/total_word}`);
+    console.log(`string length divided by total words: ${givenstr.length*total_word}`);      
+    
+}
+sentence();
+
+console.log("******************** Assignment 2 **********************");
+console.log("******************** Find greatest no among two no **********************");
+var ternary=function(){
+
+}
+ternary();
--- ternary_operator.js
+++ ternary_operator.js
@@ -1,3 +1,3 @@
-var num1=-5;
+var num1=5;
 var result=num1>0?"Positive":"negative";
 console.log(result);
