924c36ba2615d88d845d42582bb0214711783a59
277889 <112204285+277889@users.noreply.github.com>
2022-11-04 11:45:24 +0530

    hosting,local storage assignment,closure
    hosting,local storage assignment,closure

--- 15_hosting.js
+++ 15_hosting.js
@@ -1,14 +1,20 @@
 
 console.log(myname);
+//console.log(rollno);//not allowed
+//console.log(city);// not allowed
 var myname="pooja jejurkar";
+let rollno=123;     //using the let keyword not allow to access because it does not hosted
+const city="pune"   //using the let keyword not allow to access because it does not hosted
 
-
-show(); //hosted
+//  Normal function
+show(); //hosted & allowed
 function show(){
     console.log("show function");
 }
 
-display();
+//  function Expression:
+//display();  // not allow
 var display=function(){
     console.log("inside display function expression");
 }
+display();
